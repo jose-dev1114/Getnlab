035d5ff502004d458be09197228ef5dbfdd83668
jose <joseiscoding@gmail.com>
2025-10-03 07:21:08 -0700

    feat: Add Netlify deployment configuration
    - Added netlify.toml with build settings and redirects
    - Created Netlify serverless function for Remix app
    - Configured headers and caching for optimal performance

---
+++ netlify/functions/server.js
@@ -0,0 +1,8 @@
+import { createRequestHandler } from "@netlify/remix-adapter";
+import * as build from "../../build/server/index.js";
+
+export const handler = createRequestHandler({
+  build,
+  mode: process.env.NODE_ENV,
+});
+
