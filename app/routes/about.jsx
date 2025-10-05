e848870ab8a4dbb736caca02847c13d732f59819
jose <joseiscoding@gmail.com>
2025-10-04 17:04:30 -0700

    Reduce letter spacing for profile names and early access title

--- app/routes/about.jsx
+++ app/routes/about.jsx
@@ -38,6 +38,53 @@ export default function About() {
           </div>
         </div>
       </section>
+
+      <section className="about-makers">
+        <div className="about-makers-container">
+          <div className="about-makers-intro">
+            <h2 className="about-makers-title">THE MAKERS BEHIND NLAB</h2>
+            <p className="about-makers-description">
+              We're engineers and educators who started nLab in university classrooms to make electronics and robotics accessible to everyone. After years of testing with students and makers, we're bringing our proven system directly to you.
+            </p>
+          </div>
+
+          <div className="about-makers-profile">
+            <div className="about-makers-profile-text">
+              <h3 className="about-makers-profile-name">HI! I'M NICK</h3>
+              <p className="about-makers-profile-role about-makers-profile-role-nick">Co-Founder & Chief Product Officer</p>
+              <p className="about-makers-profile-bio">
+                As a professor of mechatronics, Nick noticed his students were taking turns crowded around one machine instead of building circuits – and confidence – independently.
+              </p>
+              <p className="about-makers-profile-bio">
+                He created an early version of nLab to make the teaching and learning process easier for himself and his students. The word spread about this little powerhouse, and the rest is history!
+              </p>
+            </div>
+            <div className="about-makers-profile-image">
+              <img src="/svg/img/about_first.png" alt="Nick - Co-Founder & Chief Product Officer" />
+            </div>
+          </div>
+
+          <div className="about-makers-profile about-makers-profile-reverse">
+            <div className="about-makers-profile-image">
+              <img src="/svg/img/about_second.png" alt="Angie - Co-Founder & CEO" />
+            </div>
+            <div className="about-makers-profile-text">
+              <h3 className="about-makers-profile-name">HELLO! I'M ANGIE</h3>
+              <p className="about-makers-profile-role about-makers-profile-role-angie">Co-Founder & CEO</p>
+              <p className="about-makers-profile-bio">
+                As an engineer, Angie witnessed firsthand the frustrations of learning electronics with big, clunky, outdated machines of the electronics lab. When she first encountered nLab, her world changed, and she went on to a successful career in the robotics industry, with the nLab as her constant companion.
+              </p>
+              <p className="about-makers-profile-bio">
+                After many people asked her how to get one, and realizing this could help a lot of people, she traded her desk job for an entrepreneurial role in order to bring this invention to more people around the world.
+              </p>
+            </div>
+          </div>
+
+          <div className="about-makers-footer">
+            <p>Also, we're married 😊 💕</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
