c828ff1be6710036f0eb32d348ff6a6bfd3c856c
Nick Marchuk <ndm736@users.noreply.github.com>
2026-02-02 08:31:20 -0600

    add ga4 tag
    but maybe this is the wrong spot

--- scripts/generate-index.js
+++ scripts/generate-index.js
@@ -27,6 +27,17 @@ const html = `<!DOCTYPE html>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;500;600;700&display=swap" rel="stylesheet" />
     <link href="https://fonts.cdnfonts.com/css/titling-gothic-fb-compressed" rel="stylesheet" />
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-SVNXY9CFNB"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-SVNXY9CFNB');
+</script>
+
     ${cssFiles.map(f => `<link rel="stylesheet" href="/assets/${f}" />`).join('\n    ')}
   </head>
   <body>
